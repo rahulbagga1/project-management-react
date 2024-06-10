55a190e3ecbeb1a0c2ae7b1656b92aa7dd817375
Rahul <er.rahulbagga@gmail.com>
2024-06-10 18:25:40 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import "./index.css";
-import { Button } from "./components/ui/button";
+import { Home } from "./pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Button>Shad button</Button>
+      <Home />
     </>
   );
 }
