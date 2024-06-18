e12ecc63fb505a66c4b8cebad2ade866f3c0cbdc
Rahul <er.rahulbagga@gmail.com>
2024-06-18 18:21:01 +0530

    Initial commit

--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export const ProjectList = () => {
-  return <div>ProjectList</div>;
+  return (
+    <>
+      <div className="relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5">
+        ABCD
+      </div>
+    </>
+  );
 };
